c3cd18b698bddaaea43188a418d7c91d817e50e5
JoChuan <83957962+rjyang1025@users.noreply.github.com>
2023-07-21 23:37:06 +0800

    version 1

---
+++ js/all.js
@@ -0,0 +1,12 @@
+function hamMenu() {
+    
+    let ham = document.getElementById("ham-menu");
+
+    if (ham.style.display === "block") {
+      ham.style.display = "none";
+    }
+    else {
+      ham.style.display = "block";
+    }
+    
+  }
