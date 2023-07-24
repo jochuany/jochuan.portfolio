eb2c5dd506a6ff919d214cca29f05cae103e4bd2
JoChuan <83957962+rjyang1025@users.noreply.github.com>
2023-07-24 17:51:07 +0800

    version 4

--- js/all.js
+++ js/all.js
@@ -7,4 +7,15 @@ function loadFade(){
 function hamMenu() {
   let ham = document.getElementById("ham-menu");
   ham.classList.toggle("show");
+
+
+  let lineT = document.getElementById("line1");
+  let lineM = document.getElementById("line2");
+  let lineB = document.getElementById("line3");
+
+  lineT.classList.toggle("line-1-45");
+  lineM.classList.toggle("line-2-disapear");
+  lineB.classList.toggle("line-3-45");
+
+
 }
